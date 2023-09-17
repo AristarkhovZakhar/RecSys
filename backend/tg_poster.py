97cabcaf987e7c37025fea86977f0409efcf110a
Иван Доронин <iopmanu@MacBook-Pro-Ivan-2.local>
2023-09-17 13:36:18 +0300

    Initial tg_poster

---
+++ backend/tg_poster.py
@@ -0,0 +1,11 @@
+from telethon.sync import TelegramClient
+from telethon.tl import functions
+
+
+class TGPoster:
+    def __init(
+            self,
+            client: TelegramClient,
+
+    ):
+        pass
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from services.document import DocumentService
 from services.labeler import Labeler
 from services.summarizator import YaGPTSummary
+from services.yadisk_scheduler import YaDiskScheduler
+from backend.storage.ya_disk_storage import YaDiskStorage
 
 from datetime import datetime
 
@@ -10,37 +12,46 @@ from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 
 from configs.config import MainConfig
+from configs.config import YAServices
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.models import Variable
 
 with open('configs/main_config.json') as f:
     data = json.load(f)
 
-conf = MainConfig.from_json(data)
+with open('configs/ya.json') as f:
+    y_data = json.load(f)
 
+conf = MainConfig.from_json(data)
+FILES_NUM_SCHEDULER = int(Variable.get("FILES_NUM_SCHEDULER", default_var=50))  # add it to the cfg
+
+disk_scheduler = YaDiskScheduler(YaDiskStorage(YAServices.from_dict(y_data)), FILES_NUM_SCHEDULER)
 document_service = DocumentService(conf.endpoint)
 summarizator = YaGPTSummary(conf.yandex_qa_token)
 labeler = Labeler(conf.hf_access_token)
 
 with DAG(
-    dag_id='main',
-    start_date=datetime(2023, 9, 16),
-    schedule_interval='@continuous',
+        dag_id='main',
+        start_date=datetime(2023, 9, 16),
+        schedule_interval='@continuous',
 ) as dag:
-
+    scheduler = PythonOperator(
+        task_id='run_yadisk_scheduler',
+        python_callable=disk_scheduler.run_push_service,
+    )
     news_service = PythonOperator(
-            task_id='run_push_news_to_service',
-            python_callable=document_service.run_push_news_to_service,
-        )
+        task_id='run_push_news_to_service',
+        python_callable=document_service.run_push_news_to_service,
+    )
     summarization = PythonOperator(
-            task_id='get_summary',
-            python_callable=summarizator.run_push_news_to_summarization,
-        )
+        task_id='get_summary',
+        python_callable=summarizator.run_push_news_to_summarization,
+    )
     labeler = PythonOperator(
-        task_id = 'get_labels',
+        task_id='get_labels',
         python_callable=labeler.run_push_news_to_labeler
-        )
+    )
 
-
-"Шедулер по я диску" >> news_service >> [summarization, labeler] >> "Отправка в тг"
+scheduler >> news_service  # >> [summarization, labeler] >> "Отправка в тг"
