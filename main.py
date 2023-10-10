52b115f7eb78462d3736cda26ec1358aebc99dc2
Иван Доронин <iopmanu@MacBook-Pro-Ivan-2.local>
2023-10-10 23:17:40 +0300

    dev -- ranker init version for debug

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from airflow.operators.python import PythonOperator
 from telethon import TelegramClient
 
 from configs.config import DocumentServiceConfig, YAServiceConfig, LabelerConfig
-from services.deleter import Deleter
+# from services.deleter import Deleter
 from services.document import DocumentService
-from services.labeler import Labeler
-from services.summarizator import YaGPTSummary
+# from services.labeler import Labeler
+# from services.summarizator import YaGPTSummary
 from services.ranker import Ranker
-from services.tg_poster import TGPoster
-from services.yadisk_scheduler import YaDiskScheduler
+# from services.tg_poster import TGPoster
+# from services.yadisk_scheduler import YaDiskScheduler
 
 # data_dir = "/home/parser"
 data_dir = "/opt/airflow/dags"
@@ -50,12 +50,11 @@ labeler_model_name = "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7"
 sbert_model_name = "all-MiniLM-L6-v2"
 
 document = DocumentService(document_service_config.endpoint)
-summarizator = YaGPTSummary(ya_service_config.qa_token)
-labeler = Labeler(labeler_model_name, labeler_service_config.labels)
+# summarizator = YaGPTSummary(ya_service_config.qa_token)
+# labeler = Labeler(labeler_model_name, labeler_service_config.labels)
 ranker = Ranker(model_name=sbert_model_name)
-deleter = Deleter()
-poster = TGPoster(bot, channels)
-
+# deleter = Deleter()
+# poster = TGPoster(bot, channels)
 
 #scores = {l: 1 for l in labeler_service_config.labels}
 #counts = {l: 1 for l in labeler_service_config.labels}
@@ -69,34 +68,36 @@ with DAG(
         dag_id='main',
         start_date=datetime(2023, 10, 3)
 ) as dag:
-    scheduler = PythonOperator(
-        task_id='run_push_from_disk',
-        python_callable=yadisk_scheduler.run_push_service,
-    )
-    news_service = PythonOperator(
-        task_id='run_push_news_to_service',
-        python_callable=document.run_push_news_to_service,
-    )
-    summarization = PythonOperator(
-        task_id='get_summary',
-        python_callable=summarizator.run_push_news_to_summarization,
-    )
-    labeler = PythonOperator(
-        task_id='get_labels',
-        python_callable=labeler.run_push_news_to_labeler
-    )
+    # scheduler = PythonOperator(
+    #     task_id='run_push_from_disk',
+    #     python_callable=yadisk_scheduler.run_push_service,
+    # )
+    # news_service = PythonOperator(
+    #     task_id='run_push_news_to_service',
+    #     python_callable=document.run_push_news_to_service,
+    # )
+    # summarization = PythonOperator(
+    #     task_id='get_summary',
+    #     python_callable=summarizator.run_push_news_to_summarization,
+    # )
+    # labeler = PythonOperator(
+    #     task_id='get_labels',
+    #     python_callable=labeler.run_push_news_to_labeler
+    # )
+
+    start = DummyOperator(task_id='start')
     ranker = PythonOperator(
         task_id='get_ranks',
         python_callable=ranker.run_ranking_push_poster
     )
-    poster = PythonOperator(
-        task_id='post_news',
-        python_callable=poster.run_post_messages
-    )
-    deleter = PythonOperator(
-        task_id='remove_files',
-        python_callable=deleter.delete,
-    )
+    end = DummyOperator(task_id='end')
+    # poster = PythonOperator(
+    #     task_id='post_news',
+    #     python_callable=poster.run_post_messages
+    # )
+    # deleter = PythonOperator(
+    #     task_id='remove_files',
+    #     python_callable=deleter.delete,
+    # )
 
-scheduler >> news_service >> summarization >> labeler >> ranker >> [poster,
-deleter]
+ranker
