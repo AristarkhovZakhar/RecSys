a8f742d9de85cc60328e2ae006c9250ed124a29c
root <moybropro@mail.ru>
2023-10-14 01:37:31 +0300

    last version

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from airflow.operators.python import PythonOperator
 from telethon import TelegramClient
 
 from configs.config import DocumentServiceConfig, YAServiceConfig, LabelerConfig
-# from services.deleter import Deleter
+from services.deleter import Deleter
 from services.document import DocumentService
-# from services.labeler import Labeler
-# from services.summarizator import YaGPTSummary
+from services.labeler import Labeler
+from services.summarizator import YaGPTSummary
 from services.ranker import Ranker
-# from services.tg_poster import TGPoster
-# from services.yadisk_scheduler import YaDiskScheduler
+from services.tg_poster import TGPoster
+from services.yadisk_scheduler import YaDiskScheduler
 
 # data_dir = "/home/parser"
 data_dir = "/opt/airflow/dags"
@@ -28,7 +28,6 @@ with open(f"{data_dir}/configs/api.json") as f:
 with open(f"{data_dir}/configs/channels.json") as f:
     ap = json.load(f)
     channels = ChannelsConfig.from_dict(ap)
-
 with open(f"{data_dir}/configs/document_service.json") as f:
     data = json.load(f)
     document_service_config = DocumentServiceConfig.from_dict(data)
@@ -40,9 +39,7 @@ with open(f"{data_dir}/configs/labeler.json") as f:
     labeler_service_config = LabelerConfig.from_dict(data)
 with open(f"{data_dir}/configs/bot_token.json") as f:
     bot_token = json.load(f)['bot_token']
-with open(f"{data_dir}/configs/bot_token.json") as f:
-    bot_token = json.load(f)['bot_token']
-    bot = TelegramClient("bro", api.api_id, api.api_hash).start(bot_token=bot_token)
+    bot = TelegramClient("123456popodffff", api.api_id, api.api_hash).start(bot_token=bot_token)
 
 
 storage_dir = f"{data_dir}/backend/data/"
@@ -50,52 +47,52 @@ labeler_model_name = "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7"
 sbert_model_name = "all-MiniLM-L6-v2"
 
 document = DocumentService(document_service_config.endpoint)
-# summarizator = YaGPTSummary(ya_service_config.qa_token)
-# labeler = Labeler(labeler_model_name, labeler_service_config.labels)
+summarizator = YaGPTSummary(ya_service_config.qa_token)
+labeler = Labeler(labeler_model_name, labeler_service_config.labels)
 ranker = Ranker(model_name=sbert_model_name)
-# deleter = Deleter()
-# poster = TGPoster(bot, channels)
-
-#scores = {l: 1 for l in labeler_service_config.labels}
-#counts = {l: 1 for l in labeler_service_config.labels}
-#Variable.set('RANKER_SCORES', scores)
-#Variable.set('RANKER_COUNTS', counts)
+deleter = Deleter()
+poster = TGPoster(bot, channels)
 
 ROWS_TO_PUSH = int(Variable.get("ROWS_TO_PUSH_YADISK", default_var=2))
-# yadisk_scheduler = YaDiskScheduler(storage_dir, ROWS_TO_PUSH)
+yadisk_scheduler = YaDiskScheduler(storage_dir, ROWS_TO_PUSH)
 
 with DAG(
         dag_id='main',
         start_date=datetime(2023, 10, 3)
 ) as dag:
-    # scheduler = PythonOperator(
-    #     task_id='run_push_from_disk',
-    #     python_callable=yadisk_scheduler.run_push_service,
-    # )
-    # news_service = PythonOperator(
-    #     task_id='run_push_news_to_service',
-    #     python_callable=document.run_push_news_to_service,
-    # )
-    # summarization = PythonOperator(
-    #     task_id='get_summary',
-    #     python_callable=summarizator.run_push_news_to_summarization,
-    # )
-    # labeler = PythonOperator(
-    #     task_id='get_labels',
-    #     python_callable=labeler.run_push_news_to_labeler
-    # )
+    scheduler = PythonOperator(
+        task_id='run_push_from_disk',
+        python_callable=yadisk_scheduler.run_push_service,
+        )
+    news_service = PythonOperator(
+        task_id='run_push_news_to_service',
+        python_callable=document.run_push_news_to_service,
+        )
+    summarization = PythonOperator(
+        task_id='get_summary',
+        python_callable=summarizator.run_push_news_to_summarization,
+        )
+    labeler = PythonOperator(
+        task_id='get_labels',
+        python_callable=labeler.run_push_news_to_labeler
+        )
 
     ranker = PythonOperator(
         task_id='get_ranks',
         python_callable=ranker.run_ranking_push_poster
-    )
-    # poster = PythonOperator(
-    #     task_id='post_news',
-    #     python_callable=poster.run_post_messages
-    # )
-    # deleter = PythonOperator(
-    #     task_id='remove_files',
-    #     python_callable=deleter.delete,
-    # )
+        )
+    poster = PythonOperator(
+        task_id='post_news',
+        python_callable=poster.run_post_messages
+        )
+    deleter = PythonOperator(
+        task_id='remove_files',
+        python_callable=deleter.delete,
+        )
 
-ranker
+scheduler >> news_service >> summarization >> labeler >> ranker >> [poster, deleter]
+
+try:
+    bot.disconnect()
+except OSError:
+    print('Error on disconnect')
